c6dce6ae1ba19fad14aa7ee65da903e0c4510c52
Ali Faraji <ali.faraji90@gmail.com>
2024-05-19 13:51:34 +0330

    Add overlay to default template.

--- src/templates/DefaultTemplate.tsx
+++ src/templates/DefaultTemplate.tsx
@@ -5,11 +5,13 @@ import { RealertTemplateProps } from '../types';
 const DefaultTemplate: FC<RealertTemplateProps> = ({ open, content, title, onClose, okText = 'OK' }) => {
   return (
     open && (
-      <div className={'realert'}>
-        {!!title && <div className={'realertTitle'}>{title}</div>}
-        <div className={'realertBody'}>{content}</div>
-        <div className={'realertActions'}>
-          <button onClick={onClose}>{okText}</button>
+      <div className={'realertOverlay'}>
+        <div className={'realert'}>
+          {!!title && <div className={'realertTitle'}>{title}</div>}
+          <div className={'realertBody'}>{content}</div>
+          <div className={'realertActions'}>
+            <button onClick={onClose}>{okText}</button>
+          </div>
         </div>
       </div>
     )
